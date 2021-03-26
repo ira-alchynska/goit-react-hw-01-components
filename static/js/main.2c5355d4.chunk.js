05fdc30166622f7cd205fa3e273eaa90e5dffc6e
Iryna Alchynska <68770490+ira-alchynska@users.noreply.github.com>
2021-03-26 18:21:32 +0200

    Updates

---
+++ static/js/main.2c5355d4.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],[function(o,t){}],[[0,1]]]);
+//# sourceMappingURL=main.2c5355d4.chunk.js.map
